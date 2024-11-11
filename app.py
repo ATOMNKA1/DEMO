3c3aade79a4f002ec6f8f18fe23432d3595e6e64
unknown <nikita.gelver7777777@gmail.com>
2024-11-11 19:20:38 +0500

    Создание заявки со всеми параметрами

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+class Order(BaseModel):
+    number : int
+    startDate : datetime.date
+    device : str
+    problemType : str
+    description : str
+    client : str
+    status : str
+
+
 app = FastAPI()
 
 @app.get("/")
